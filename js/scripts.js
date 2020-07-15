9369fe2cd8f3c6f7ea3d14fdfdaa7a408b2b0dab
Ondicho <ondicho53@gmail.com>
2020-07-15 16:56:50 +0300

    this commit adds addition and subtraction function to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var add = function(number1,	number2)	{
+return number1	+	number2;
+};
+var number1=parseInt(prompt("enter number1"));
+var number2=parseInt(prompt("enter number2"));
+alert(add(number1,  number2));
+var subtract=function(number1,	number2)	{
+return	number1	-	number2;
+};
+var number1=parseInt(prompt("enter number1"));
+var number2=parseInt(prompt("enter number2"));
+alert(subtract(number1,	number2));
