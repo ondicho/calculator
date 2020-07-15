f747f0faa6b5002ee546e31bc6f776593e7aafca
Ondicho <ondicho53@gmail.com>
2020-07-15 17:13:02 +0300

    this commit adds basic structure to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,25 +4,29 @@ return number1	+	number2;
 };
 var number1=parseInt(prompt("enter number1"));
 var number2=parseInt(prompt("enter number2"));
-alert(add(number1,  number2));
+var result=add(number1,  number2);
+alert(result);
 //subtraction
 var subtract=function(number1,	number2)	{
 return	number1	-	number2;
 };
 var number1=parseInt(prompt("enter number1"));
 var number2=parseInt(prompt("enter number2"));
-alert(subtract(number1,	number2));
+var result=subtract(number1,	number2);
+alert(result);
 //multiplication
 var multiply=function(number1,  number2)  {
   return number1  * number2;
 };
 var number1=parseInt(prompt("enter number1"));
 var number2=parseInt(prompt("enter number2"));
-alert(multiply(number1, number2));
+var result=multiply(number1, number2);
+alert(result);
 //division
 var divide=function(number1,  number2)  {
   return number1  / number2;
 };
 var number1=parseInt(prompt("enter number1"));
 var number2=parseInt(prompt("enter number2"));
-alert(divide(number1, number2));
+var result=divide(number1, number2)
+alert(result);
