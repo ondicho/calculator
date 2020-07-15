1fefed18bff0fe9002bf10de34e30e5f001bc001
Ondicho <ondicho53@gmail.com>
2020-07-15 17:05:16 +0300

    this commit adds multiplication and division functions to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
+//addition function
 var add = function(number1,	number2)	{
 return number1	+	number2;
 };
 var number1=parseInt(prompt("enter number1"));
 var number2=parseInt(prompt("enter number2"));
 alert(add(number1,  number2));
+//subtraction
 var subtract=function(number1,	number2)	{
 return	number1	-	number2;
 };
 var number1=parseInt(prompt("enter number1"));
 var number2=parseInt(prompt("enter number2"));
 alert(subtract(number1,	number2));
+//multiplication
+var multiply=function(number1,  number2)  {
+  return number1  * number2;
+};
+var number1=parseInt(prompt("enter number1"));
+var number2=parseInt(prompt("enter number2"));
+alert(multiply(number1, number2));
+//division
+var divide=function(number1,  number2)  {
+  return number1  / number2;
+};
+var number1=parseInt(prompt("enter number1"));
+var number2=parseInt(prompt("enter number2"));
+alert(divide(number1, number2));
